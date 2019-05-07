329daa82133ee4bc2a4ed48d8a3ca7ab32000fab
Nikita Titov <nekit94-08@mail.ru>
2019-05-07 04:38:29 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -168,9 +168,9 @@ def generate_doxygen_xml(app):
     """
     commands = """
     export PATH="/home/docs/.conda/bin:$PATH"
+    conda activate base
     conda install -y -q gfortran_linux-64 gxx_linux-64
-    /home/docs/.conda/etc/conda/activate.d/activate-gfortran_linux-64.sh
-    /home/docs/.conda/etc/conda/activate.d/activate-gxx_linux-64.sh
+    conda activate base
     R_VER=3.6.0
     wget -q https://cran.r-project.org/src/base/R-3/R-$R_VER.tar.gz
     tar -xzf R-$R_VER.tar.gz
