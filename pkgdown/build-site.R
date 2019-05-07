e2dbbd7237fed2c38ed41dac35eef0e7e0453cc7
Nikita Titov <nekit94-08@mail.ru>
2019-05-08 01:16:21 +0300

    Create build-site.R

---
+++ pkgdown/build-site.R
@@ -0,0 +1,29 @@
+setwd("/home/docs/checkouts/readthedocs.org/user_builds/lightgbm/checkouts/docs/R-package")
+devtools::document()
+
+if (!dir.exists("./docs")) {
+  dir.create("./docs")
+}
+print("========================building pkgdown site====================================")
+# options(pkgdown.internet=FALSE)
+library(pkgdown)
+
+clean_site()
+init_site()
+build_home(quiet = FALSE,preview = FALSE)
+build_reference(preview = FALSE)
+# # to-do
+# build_articles(preview = FALSE)
+# build_tutorials(preview = FALSE)
+# build_news(preview = FALSE)
+
+# # don't work
+# pkgdown::build_site(pkg = ".", examples = FALSE, document = TRUE,
+#                     run_dont_run = TRUE, seed = 1014, lazy = FALSE,
+#                     override = list(), preview = NA, new_process = FALSE)
+
+# add artifacts
+# devtools::build(binary = TRUE, args = c("--preclean", "--no-multiarch", "--keep-empty-dirs"))
+# version <- gsub("Version: ","",grep("Version: ",readLines(con = file.path(".", "DESCRIPTION")),value = TRUE))
+# file.copy(file.path("..", sprintf("lightgbm_%s.tar.gz", version)),".")
+# file.copy(file.path("..", sprintf("lightgbm_%s.zip", version)),".")
