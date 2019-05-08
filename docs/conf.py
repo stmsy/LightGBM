47107df904924412ac44a072ff15070e5facd8c5
Nikita Titov <nekit94-08@mail.ru>
2019-05-08 08:08:33 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -174,9 +174,9 @@ def generate_doxygen_xml(app):
     export TAR=/bin/tar
     echo "R_LIBS=$HOME/R_LIBS" > $HOME/.Renviron
     echo 'options(repos = "https://cran.rstudio.com")' > $HOME/.Rprofile
-    Rscript -e 'install.packages("pkgdown", dependencies = TRUE)'
     cd /home/docs/checkouts/readthedocs.org/user_builds/lightgbm/checkouts/docs
     Rscript -e "devtools::install_version('roxygen2', version = '6.0.1')"
+    Rscript -e 'install.packages("pkgdown", dependencies = TRUE)'
     Rscript -e "sessionInfo()"
     Rscript pkgdown/build-site.R
     """
