096d4152ef428940d0ce1a82e8635c915b929bee
StrikerRUS <nekit94-12@hotmail.com>
2019-05-10 00:40:50 +0300

    test

--- .ci/test.sh
+++ .ci/test.sh
@@ -67,7 +67,7 @@ if [[ $TASK == "if-else" ]]; then
 fi
 
 conda install -q -y -n $CONDA_ENV matplotlib numpy pandas psutil pytest python-graphviz scikit-learn scipy
-ll lib*omp*
+#conda install -q -y -n $CONDA_ENV nomkl
 if [[ $OS_NAME == "macos" ]] && [[ $COMPILER == "clang" ]]; then
     sudo ln -sf `ls -d "$(brew --cellar libomp)"/*/lib`/* $CONDA_PREFIX/lib || exit -1  # fix "OMP: Error #15: Initializing libiomp5.dylib, but found libomp.dylib already initialized." (OpenMP library conflict due to conda's MKL)
 fi
@@ -138,7 +138,6 @@ fi
 make _lightgbm -j4 || exit -1
 
 cd $BUILD_DIRECTORY/python-package && python setup.py install --precompile --user || exit -1
-pytest $BUILD_DIRECTORY/tests || exit -1
 
 if [[ $TASK == "regular" ]]; then
     if [[ $AZURE == "true" ]]; then
